15cf4b94d1a3458cc794668c7b5406308907ed20
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-05-01 21:50:32 +0200

    Created app scaffold

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const hbs = require("express-handlebars").create({
+  extname: ".hbs",
+});
+
+const PORT = 3000;
+
+const app = express();
+
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/static/", express.static("static"));
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
