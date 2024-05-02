2b881d5069c7debed2a6f02b126777fd86221440
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-05-02 13:41:21 +0200

    Room creation page

--- controllers/createController.js
+++ controllers/createController.js
@@ -1,7 +1,18 @@
+const { create } = require("../services/accommodationService");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
   res.render("create", { title: "Host new Accommodation" });
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const result = await create(req.body);
+    res.redirect("/catalog/" + result.id);
+  } catch (error) {
+    res.render("create", { title: "Request Error creating Accommodation" });
+  }
 });
 
 module.exports = router;
