e44a282bfa80992b1739ac486ecbf8f4197f9c99
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-05-05 14:51:22 +0200

    Implement User model and auth service

--- config/routes.js
+++ config/routes.js
@@ -3,14 +3,15 @@ const catalogController = require("../controllers/catalogController");
 const createController = require("../controllers/createController");
 const defaultController = require("../controllers/defaultController");
 const facilityController = require("../controllers/facilityController");
-const authController = require('../controllers/authController');
+const authController = require("../controllers/authController");
+const { hasUser, isGuest } = require("../middlewares/guards");
 
 module.exports = (app) => {
   app.use(homeController);
   app.use("/catalog", catalogController);
-  app.use("/create", createController);
+  app.use("/create", hasUser(), createController);
   app.use("/facility", facilityController);
-  app.use('/auth', authController);
-  
+  app.use("/auth", isGuest(), authController);
+
   app.all("*", defaultController);
 };
