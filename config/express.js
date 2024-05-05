9ad4ab236ae0000f31a88c6664ab3d481ce9f553
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-05-05 07:11:26 +0200

    Add mock auth controller and service

--- config/express.js
+++ config/express.js
@@ -3,7 +3,11 @@ const hbs = require("express-handlebars").create({
   extname: ".hbs",
 });
 
+const cookieParser = require('cookie-parser');
 const defaultTitle = require("../middlewares/defaultTitle");
+const auth = require('../middlewares/auth');
+
+const jwtSecret = '9a328ncaiowjdf';
 
 module.exports = (app) => {
   app.engine(".hbs", hbs.engine);
@@ -11,6 +15,8 @@ module.exports = (app) => {
 
   app.use(express.urlencoded({ extended: true }));
   app.use("/static", express.static("static"));
+  app.use(cookieParser());
+  app.use(auth(jwtSecret));
 
   app.use(defaultTitle("SoftUni Accommodation"));
 };
