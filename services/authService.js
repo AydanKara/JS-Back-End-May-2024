68d1231fb49660716baab20cc296b315e83cf0e9
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-05-05 14:58:26 +0200

    Add index to User model

--- services/authService.js
+++ services/authService.js
@@ -1,48 +1,49 @@
-const bcrypt = require('bcrypt');
-const User = require('../models/User');
-
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
 async function register(username, password) {
-    // check if username is taken
-    const existing = await User.findOne({ username: { $regex: new RegExp(username), $options: 'i' } });
-    if (existing) {
-        throw new Error('Username is taken');
-    }
+  // check if username is taken
+  const existing = await User.findOne({ username });
+  if (existing) {
+    throw new Error("Username is taken");
+  }
 
-    // hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    // create and save user
-    const user = await User.create({
-        username,
-        hashedPassword
-    });
+  // create and save user
+  const user = await User.create({
+    username,
+    hashedPassword,
+  });
 
-    // return user data
-    return {
-        username,
-        roles: user.roles
-    };
+  // return user data
+  return {
+    username,
+    roles: user.roles,
+  };
 }
 
 async function login(username, password) {
-    const user = await User.findOne({ username: { $regex: new RegExp(username), $options: 'i' } });
-    if (!user) {
-        throw new Error('Incorrect username or password');
-    }
+  const user = await User.findOne({
+    username,
+  }).collation({ locale: "en", strength: 2 });
+  if (!user) {
+    throw new Error("Incorrect username or password");
+  }
 
-    const match = await bcrypt.compare(password, user.hashedPassword);
-    if (!match) {
-        throw new Error('Incorrect username or password');
-    }
+  const match = await bcrypt.compare(password, user.hashedPassword);
+  if (!match) {
+    throw new Error("Incorrect username or password");
+  }
 
-    return {
-        username: user.username,
-        roles: user.roles
-    };
+  return {
+    username: user.username,
+    roles: user.roles,
+  };
 }
 
 module.exports = {
   login,
-  register
+  register,
 };
